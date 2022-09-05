582ab56a09c09e3061ac696e1bd5b98912628dea
nicostray <nicolascorrea.alfaro@gmail.com>
2022-09-05 11:13:06 -0300

    add link to repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/footer.css'
 const Footer = () => {
   return (
     <footer>
-        <a href="/"><i className="fa-brands fa-square-github"></i> Github</a>
+        <a href="https://github.com/nicostray/colors-frontend" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-square-github"></i> Github</a>
     </footer>
   )
 }
